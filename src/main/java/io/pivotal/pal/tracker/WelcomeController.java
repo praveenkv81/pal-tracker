147a41b4fc7928279c3e71d7a0732d6b6887ba05
Veerabadran <Praveen.Kumar.X.Veerabadran.-ND@disney.com>
2020-05-18 15:09:33 -0400

    PAL praveen veerabadran - assignment 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 	
+	@Value("${welcome.message}")
+	String hello;
+	
+	@Value("${testMessage:praveenkumar}")
+	String testMsg;
+	
 	@GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return "Hello " + hello ;
     }
 }
